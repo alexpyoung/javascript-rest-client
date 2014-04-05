864f8657407b43216d4f41047e92dbad8c152bcf
Alex <alexyoung1992@gmail.com>
2014-04-05 12:02:19 -0700

    Implemented JavaScript Module Pattern

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,2 +1,14 @@
-var APP = APP || {
-};
+/* @Module: APP
+ * The public interface module organized into various,
+ * loosely-coupled, augmentable sub-modules.
+ *
+ * @param: NONE
+ * @return: public interface
+ */
+var APP = (function () {
+    var app = {}; // public interface to return
+
+        // Add methods like so: app.method = function () {};
+
+    return app;
+}());
