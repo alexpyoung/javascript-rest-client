824c11f7cb82b7f3d416dd49cea01259f9b8ad8f
Alex <alexyoung1992@gmail.com>
2014-04-07 23:56:14 -0700

    Refactored mock responses - they are now dynamic and can modify data state

--- tests/mock-server.js
+++ tests/mock-server.js
@@ -1,53 +1,66 @@
+/*global APP */
+
+var objects = [
+    {
+        "id": 0,
+        "color": "red"
+    },
+    {
+        "id": 1,
+        "color": "red"
+    },
+    {
+        "id": 2,
+        "color": "green"
+    },
+    {
+        "id": 3,
+        "color": "green"
+    },
+    {
+        "id": 4,
+        "color": "blue"
+    },
+    {
+        "id": 5,
+        "color": "blue"
+    }
+],
+    objectsRegex = /^\/objects$/,
+    objectWithIdRegex = /^\/objects\/([\d]+)$/;
+
 // Mock response for GET /objects
+/*jslint unparam: true */
 $.mockjax({
-    "url": "/objects",
+    "url": objectsRegex,
     "type": "GET",
     "status": 200,
-    "responseText": {
-        "objects": [
-            {
-                "id": 0,
-                "color": "red"
-            },
-            {
-                "id": 1,
-                "color": "red"
-            },
-            {
-                "id": 2,
-                "color": "green"
-            },
-            {
-                "id": 3,
-                "color": "green"
-            },
-            {
-                "id": 4,
-                "color": "blue"
-            },
-            {
-                "id": 5,
-                "color": "blue"
-            }
-        ],
-        "size": 6
+    "response": function (settings) {
+        this.responseText = {
+            "objects": objects,
+            "size": objects.length
+        };
+        return this;
     }
 });
+/*jslint unparam: false */
 
 // Mock response for GET /objects/:id
 $.mockjax({
-    "url": "/objects/0",
+    "url": objectWithIdRegex,
+    "urlParams": ["objectID"],
     "type": "GET",
     "status": 200,
-    "responseText": {
-        "id": 0,
-        "color": "red"
+    "response": function (settings) {
+        var objectID = Number(settings.urlParams.objectID);
+        this.responseText = objects[objectID];
+        return this;
     }
 });
 
 // Mock response for POST /objects
 $.mockjax({
-    "url": "/objects",
+    "url": objectsRegex,
     "type": "POST",
     "status": 200,
     "responseText": {}
@@ -55,7 +68,8 @@ $.mockjax({
 
 // Mock response for PUT /objects/:id
 $.mockjax({
-    "url": "/objects/0",
+    "url": objectWithIdRegex,
+    "urlParams": ["objectID"],
     "type": "PUT",
     "status": 200,
     "responseText": {}
@@ -63,8 +77,23 @@ $.mockjax({
 
 // Mock response for DELETE /objects/:id
 $.mockjax({
-    "url": "/objects/0",
+    "url": objectWithIdRegex,
+    "urlParams": ["objectID"],
     "type": "DELETE",
     "status": 200,
-    "responseText": {}
+    "response": function (settings) {
+        var objectID = Number(settings.urlParams.objectID),
+            i,
+            length;
+        // debugger;
+        for (i = 0, length = objects.length; i < length; i++) {
+            if (objects[i].id === objectID) {
+                objects.splice(i, 1);
+                APP.Controller.index();
+                break;
+            }
+        }
+        this.responseText = {};
+        return this;
+    }
 });
