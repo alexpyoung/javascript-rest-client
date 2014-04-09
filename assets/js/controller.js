ec7537e8679e9c8e4b7be4e28dfdcb7eb0e90248
Alex <alexyoung1992@gmail.com>
2014-04-08 18:13:18 -0700

    Added Controller.delete action and tests

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -10,7 +10,9 @@
 APP.Controller = (function ($, Handlebars, service) {
     var controller = {}, // Public interface to return
         $objectsElement = $("#objects"), // Cache DOM query
-        appendObjects, // Private method
+        indexObjects, // Private method
+        deleteObject, // Private method
+        objectButtonHandler, // Private method
         ControllerException; // Exception object for error handling
 
     /* ATTN: COMMENT OUT IN PRODUCTION
@@ -19,21 +21,23 @@ APP.Controller = (function ($, Handlebars, service) {
      */
     controller.counter = 0;
 
-    /* @method appendObjects
+    /* @method indexObjects
      * Uses Handlebars client-side templating to render
      * a JSON array of objects and append it to the DOM
      *
      * @param [REQUIRED] JSON array of objects
      * @param [REQUIRED] DOM element to append HTML
      */
-    appendObjects = function (objects, $domElement) {
+    indexObjects = function (objects, $domElement) {
         // Error handling
         if (!Array.isArray(objects)) {
-            throw new ControllerException(".appendObjects - Expected array");
+            throw new ControllerException(".indexObjects - Expected array");
         }
         if (!($domElement instanceof $)) {
-            throw new ControllerException(".appendObjects - Expected jQuery object");
+            throw new ControllerException(".indexObjects - Expected jQuery object");
         }
+
+        $domElement.empty();
 
         var objectTemplate = Handlebars.compile($("#object-template").html()),
             htmlWithData;
@@ -46,6 +50,31 @@ APP.Controller = (function ($, Handlebars, service) {
         /*jslint unparam: false */
     };
 
+    /* @method deleteObject
+     * Removes DOM node containing the object identified by given id
+     *
+     * @param [REQUIRED] ID of object to be removed
+     * @param [REQUIRED] DOM element containing objects
+     */
+    deleteObject = function (id, $domElement) {
+    // Error handling
+        if (!(/^\d+$/).test(id) || id < 0) {
+            throw new ControllerException(".deleteObject - Invalid ID: " + id);
+        }
+        if (!($domElement instanceof $)) {
+            throw new ControllerException(".deleteObject - Expected jQuery object");
+        }
+
+        var dataIdAttribute = "li[data-id=\"" + id + "\"]";
+        $domElement.find(dataIdAttribute).remove();
+    };
+
+    objectButtonHandler = function () {
+        var id = $(this).attr("data-id");
+
+        controller.delete(id);
+    };
+
     /* @method init
      * Constructor for controller, invoked with every controller-action
      * attribute pair in HTML, and by data-controller attribute 
@@ -55,6 +84,8 @@ APP.Controller = (function ($, Handlebars, service) {
      */
     controller.init = function () {
         controller.counter++; // ATTN: COMMENT OUT IN PRODUCTION
+
+        $objectsElement.on('click', '.object button', objectButtonHandler);
     };
 
     /* @method index
@@ -67,7 +98,7 @@ APP.Controller = (function ($, Handlebars, service) {
 
         promise.done(function (data) {
             var objects = data.objects;
-            appendObjects(objects, $objectsElement);
+            indexObjects(objects, $objectsElement);
         });
         /*jslint unparam: true */
         promise.fail(function (jqXHR, status, error) {
@@ -76,6 +107,32 @@ APP.Controller = (function ($, Handlebars, service) {
         /*jslint unparam: false */
 
         controller.counter++; // ATTN: COMMENT OUT IN PRODUCTION
+    };
+
+    /* @method delete
+     * Removes object from DOM and requests DELETE /objects/:id
+     *
+     * @param [REQUIRED] ID of object to be deleted
+     */
+    controller.delete = function (id) {
+        // Error handling 
+        if (!(/^\d+$/).test(id)) { // Is not number
+            throw new ControllerException(".delete - ID is not a number: " + id);
+        }
+        if (id < 0) { // Invalid range
+            throw new ControllerException(".at - ID out of range: " + id);
+        }
+
+        var promise = service.query.DELETE("/objects/" + id);
+        console.log(promise);
+        promise.done(function () {
+            deleteObject(id, $objectsElement);
+        });
+        /*jslint unparam: true */
+        promise.fail(function (jqXHR, status, error) {
+            throw new ControllerException(".delete - DELETE /objects/" + id + " failed: " + status + ", " + error);
+        });
+        /*jslint unparam: false */
     };
 
     // Custom exception for APP.Controller
@@ -87,6 +144,8 @@ APP.Controller = (function ($, Handlebars, service) {
     };
 
     // ATTN: Begin test | COMMENT OUT IN PRODUCTION
+    controller.indexObjects = indexObjects;
+    controller.deleteObject = deleteObject;
     controller.ControllerException = ControllerException;
     // End test | END COMMENT
 
--- tests/controller.test.js
+++ tests/controller.test.js
@@ -1,18 +1,49 @@
-/*global jQuery, APP, module, asyncTest, ok, start */
+/*global jQuery, APP, module, test, asyncTest, throws, ok, start */
 
 (function ($, Controller) {
     "use strict";
+
+    var $objects = $("#objects");
+
     module("Controller", {
         teardown: function () {
             $("#objects").empty();
         }
     });
 
+
     asyncTest("index", function () {
-        ok($("#objects").is(":empty"), "DOM is empty");
-        Controller.index();
+        ok($objects.is(":empty"), "DOM is empty");
+        try {
+            Controller.index();
+        } catch (exception) {
+            console.log("Error in Controller.index test: " + exception);
+        }
+
         setTimeout(function () {
-            ok(!$("#objects").is(":empty"), "DOM is populated");
+            ok(!$objects.is(":empty"), "DOM is populated");
+            start();
+        }, 1000);
+    });
+
+    asyncTest("delete", function () {
+        $objects.append("<li data-id=\"0\"></li>");
+
+        throws(function () { Controller.delete(-1); },
+            Controller.ControllerException,
+            "Deleting an object with invalid index throws error");
+
+        var id = 0; // Testing DELETE /objects/0
+            // liElement = $objects.has("li[data-id=\"" + id + "\"]"); // Actually an array of elements matching jQuery selector
+
+        try {
+            Controller.delete(id);
+        } catch (exception) {
+            console.log("Error in Controller.delete test: " + exception);
+        }
+
+        setTimeout(function () {
+            ok($objects.is(":empty"), "<li data-id=\"" + id + "\"> removed");
             start();
         }, 1000);
     });
